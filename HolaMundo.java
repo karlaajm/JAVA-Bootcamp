2be541ec02edc8267fef6c0d7338e3847e9fea53
Karla Jiménez Millar <kjmillar0@gmail.com>
2024-09-28 15:19:38 -0300

    ADD MiInformacion

---
+++ HolaMundo.java
@@ -0,0 +1,11 @@
+public class HolaMundo{
+    public static void main (String[] args){
+        System.out.println("Hola Mundo.");
+        System.out.println("Ahora comenzamos nuestro desarrollo en Java.");
+    }
+}
+
+/* Para compliar por la terminal el archivo.java se utiliza "javac nombreArchivo.java"
+ * Para correr el archivo compilado (archivo.class) se utiliza "java nombreArchivo"
+ */
+
---
+++ MiIndormacion.java
@@ -0,0 +1,8 @@
+public class MiIndormacion {
+    public static void main(String[] args){
+        System.out.println("Hola, mi nombre es Karla Jiménez");
+        System.out.println("Tengo 25 años de edad");
+        System.out.println("Soy de Chile y vivo en la ciudad de Concepción");
+        System.out.println("Me gusta mucho programar y jugar lol");
+    }
+}
